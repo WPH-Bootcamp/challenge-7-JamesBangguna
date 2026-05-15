ee45692f04f3e7c0f0ae24d33745b3585c67ddaa
JamesBangguna <jbangguna@gmail.com>
2026-05-15 10:18:47 +0800

    Add Todo interface definition

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,6 @@
-// TODO: Definisikan tipe data untuk To-Do item di sini
-// Hint: To-Do sebaiknya memiliki id, text, dan status completed
-
-// TODO: Buat interface untuk To-Do item
-
-// TODO: Buat tipe untuk status To-Do (active/done)
-
-// TODO: Buat tipe untuk fungsi-fungsi yang akan digunakan
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  createdAt: string;
+}
